46cc565a5dc76d4acca3c224b19307dc51fe006e
Jet-Rex <v.bazanov7@gmail.com>
2025-11-21 06:19:10 +0700

    Update Частота повторений.cpp

--- Частота повторений.cpp
+++ Частота повторений.cpp
@@ -3,13 +3,13 @@
 * Название: Вариант №1 *
 *************************/
 
-using namespace std;
-
 #include <iostream>
 #include <fstream>
 #include <map>
 #include <string>
 #include <cctype>
+
+using namespace std;
 
 int main() {
   ifstream file("filename.txt");
@@ -24,35 +24,36 @@ int main() {
 
   map<char, int> letterCount;
 
-  for (size_t i = 0; i < text.size(); i++) {
-    if (isalpha(text[i])) {
-      char lowerChar = tolower(text[i]);
+  for (size_t index = 0; index < text.size(); ++index) {
+    if (isalpha(text[index])) {
+      char lowerChar = tolower(text[index]);
       letterCount[lowerChar]++;
     }
   }
   
   map<string, int> pairCount;
 
-  for (size_t i = 0; i + 1 < text.size(); i++) {
-    if (isalpha(text[i]) && isalpha(text[i + 1])) {
-      char c1 = tolower(text[i]);
-        char c2 = tolower(text[i + 1]);
-          std::string pair = "";
+  for (size_t index = 0; index + 1 < text.size(); ++index) {
+    if (isalpha(text[index]) && isalpha(text[indexi + 1])) {
+      char c1 = tolower(text[index]);
+        char c2 = tolower(text[index + 1]);
+          string pair = "";
           pair += c1;
           pair += c2;
           pairCount[pair]++;
     }
   }
 
-  cout << "Частоты букв:\n";
-  for (auto const& item : letterCount) {
+  cout << "Character frequencies:\n";
+  for (pair <char, int> item : letterCount) {
     cout << item.first << ": " << item.second << endl;
   }
 
-  cout << "Частоты двухбуквенных сочетаний:\n";
-  for (auto const& item : pairCount) {
+  cout << "Two-character frequencies:\n";
+  for (pair <char, int> item : pairCount) {
     cout << item.first << ": " << item.second << endl;
   }
 
   return 0;
+
 }
